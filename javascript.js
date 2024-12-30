8011d113ae27778170bec0272544e96aaaac2835
HaileabWolde <womahaileab1428@gmail.com>
2024-12-30 22:05:43 +0300

    Added a Random Color Generator while hovering row to be able to generate random color while hovering

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
 let container = document.querySelector(".sketchPad");
 let color = document.querySelector(".color");
 let selectedColor = color.value;
+let truevalue = false;
+
 color.addEventListener("input", ()=>{
     selectedColor = color.value;
 })
-
+color.addEventListener("click", ()=>{
+    truevalue = false;
+})
 function startGrid(input){
     container.innerHTML = ''; // Clear any existing grid
     let cellHeight = 100 / input; 
@@ -16,9 +20,17 @@ function startGrid(input){
             let row = document.createElement("div");
             row.classList.add('row');
             row.style.width = `${cellHeight}%`; 
-            row.addEventListener("mouseover", ()=>{
-                row.style.backgroundColor = selectedColor;
-            });
+            function changeRow(){
+                function generateRandomColor(){
+                    let r = Math.floor(Math.random() * 256);
+                    let g = Math.floor(Math.random() * 256);
+                    let b = Math.floor(Math.random() * 256);
+                    let rgb = `rgb(${r}, ${g}, ${b})`;
+                    return rgb;
+                }
+                row.style.backgroundColor = truevalue ? generateRandomColor() : selectedColor;
+            }
+            row.addEventListener("mouseover", changeRow);
             column.appendChild(row);
         }
         container.appendChild(column);
@@ -28,10 +40,15 @@ function startGrid(input){
 let submit = document.getElementById("mySubmit");
 let Eraser = document.getElementById("eraser");
 let clear = document.getElementById("clear");
+let Random = document.getElementById("random");
+Random.addEventListener("click", ()=>{
+    truevalue = true;
+})
 clear.addEventListener("click", ()=>{
     container.innerHTML = ''; // Clear any existing grid
 })
 Eraser.addEventListener("click", ()=>{
+    truevalue = false;
     selectedColor = `#FFFFFF`;
 });
 submit.addEventListener("click", function () {
